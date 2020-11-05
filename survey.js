d8a5296930bb76b3414cd531ed16e13fd16f62ba
deonleer <deon.leer96@gmail.com>
2020-11-05 07:59:52 +0000

    full survey function

--- survey.js
+++ survey.js
@@ -5,8 +5,28 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What is ya name????? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+let answers = [];
 
-  rl.close();
+rl.question('What\'s your name? ', (answer) => {
+  answers.push(answer);
+  rl.question('What activity do you like doing?', (answer) => {
+    answers.push(answer);
+    rl.question('What do you listen to while doing that?', (answer) => {
+      answers.push(answer);
+      rl.question('Which meal is your favourite', (answer) => {
+        answers.push(answer);
+        rl.question('What\'s your favourite thing to eat for that meal', (answer) => {
+          answers.push(answer);
+          rl.question('What is your favorite sport?', (answer) => {
+            answers.push(answer);
+            rl.question('What is your superpower?', (answer) => {
+              answers.push(answer);
+              rl.close();
+              console.log(`${answers[0]} likes to ${answers[1]} while listening to ${answers[2]}. Their favorite meal is ${answers[4]} for ${answers[3]}. Their favorite sport is ${answers[5]} and their superpower is ${answers[6]}`);
+            })
+          })
+        })
+      })
+    })
+  })
 });
